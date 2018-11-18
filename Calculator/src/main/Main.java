fa4bf330cab086fa0634696cfafc0a33c75000e8
origi <origi@172.24.128.37>
2018-11-18 13:15:30 -0500

    Created buttons and text field

--- Calculator/src/main/Main.java
+++ Calculator/src/main/Main.java
@@ -1,94 +1,73 @@
 package main;
-import java.util.Arrays;
 
-import datatypes.Matrix;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application
 {
-	public Stage window;
-	public Scene scene;
-	public Group root;
+	private Stage window;
+	private Scene scene;
+	private Group root;
+	private GridPane gridPane;
 	
 	public static void main(String[] args)
 	{
-		int[][] array =
-		{ 
-			{1, 2, 4},
-			{-3, 0, 3},
-			{-4, -2, -1},
-		};
-		
-//		System.out.println(Determinants.calculateDeterminant(array));
-//		int[][] array2 = Determinants.getArrayAfterDelete(array, 0, 2);
-		
-		
-		int[][] a1 = 
-		{
-			{0, 1, 3},
-			{1, 1, 4},
-			{2, 2, 2}
-		};
-		int[][] a2 = 
-		{
-			{1, 1, 3},
-			{1, 2, 4},
-			{4, 8, 1}
-		};
-		
-		int[][] a3 =
-		{
-			{-2, -4, 9},
-			{-4, -8, 17},
-			{1, 2, -4}
-		};
-		
-		int x = 14 * 3;
-		System.out.println(x);
-		Matrix m3 = new Matrix(a3);
-		System.out.println(m3.getRREF());
-		//m3.reducedRowEchelonForm();
-		//System.out.println(m3.getInverseMatrix().getInverseMatrix());
-		//System.out.println(Matrix.combineMatrix(new Matrix(a1), new Matrix(a2)));
-		//System.out.println(Matrix.getIdentityMatrix(100));
-		
-		
-//		for(double[] row: a3)
-//		{
-//			for(double col: row)
-//			{
-//				System.out.print(col + " ");
-//			}
-//			System.out.println();
-//		}
-		
-//		double[][] d = new double[3][3];
-//		Arrays.fill();
-//		System.out.println("What is the default" + d[0][0]);
-		
 		launch(args);
 	}
-
-	@Override
-	public void start(Stage arg0) throws Exception
+	
+	public void start(Stage arg0) throws Exception 
 	{
 		window = arg0;
-//		root = new Group();
-//		scene = new Scene(root, 1000, 500);
-//		
-//		Button btn1 = new Button("Hello Boys");
-//		root.getChildren().add(btn1);
-//		BorderPane fxmlBP = FXMLLoader.load(getClass().getResource("/CalcDisplay.fxml"));
-//		scene.setRoot(fxmlBP);
-//		
-//		window.setScene(scene);
+		window.setTitle("Calculator");
+		
+		VBox vbox = new VBox();
+		
+		gridPane = new GridPane();
+		root = new Group();
+		root.getChildren().add(vbox);
+		
+		scene = new Scene(root, 640, 480);
+		
+		TextArea textArea = new TextArea();
+		vbox.getChildren().add(textArea);
+		vbox.getChildren().add(gridPane);
+		
+		createButtons();
+		
+		window.setScene(scene);
 		window.show();
-		window.close();
+	}
+	
+	public void createButtons()
+	{
+		// numbers, row0
+		Button sev = new Button("7"); gridPane.add(sev, 0, 0);
+		Button eig = new Button("8"); gridPane.add(eig, 1, 0);
+		Button nin = new Button("9"); gridPane.add(nin, 2, 0);
+		// numbers, row1
+		Button fou = new Button("4"); gridPane.add(fou, 0, 1);
+		Button fiv = new Button("5"); gridPane.add(fiv, 1, 1);
+		Button six = new Button("6"); gridPane.add(six, 2, 1);
+		// numbers, row2
+		Button one = new Button("1"); gridPane.add(one, 0, 2);
+		Button two = new Button("2"); gridPane.add(two, 1, 2); 
+		Button thr = new Button("3"); gridPane.add(thr, 2, 2);
+		// row3
+		Button zer = new Button("0"); gridPane.add(zer, 0, 3);
+		Button dot = new Button("."); gridPane.add(dot, 1, 3);
+		Button equ = new Button("="); gridPane.add(equ, 2, 3);
+		
+		// operators
+		Button div = new Button("÷"); gridPane.add(div, 3, 0);
+		Button mul = new Button("x"); gridPane.add(mul, 3, 1);
+		Button sub = new Button("-"); gridPane.add(sub, 3, 2);
+		Button add = new Button("+"); gridPane.add(add, 3, 3);
 	}
 }
